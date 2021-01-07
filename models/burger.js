48f9943d32079e79202063d4b3c1fa6450e89da5
cdunahee36 <dunahee10@gmail.com>
2021-01-06 18:41:00 -0600

    Completed the model.

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,27 @@
+var orm = require("../config/orm.js");
 
+var burger = {
+    all: function (done) {
+        orm.all("burger", function (res) {
+            done(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, done) {
+        orm.create("burger", cols, vals, function (res) {
+            done(res);
+        });
+    },
+    update: function (objColVals, condition, done) {
+        orm.update("burger", objColVals, condition, function (res) {
+            done(res);
+        });
+    },
+    delete: function (condition, done) {
+        orm.delete("burger", condition, function (res) {
+            done(res);
+        });
+    }
+};
+
+module.exports = burger;
